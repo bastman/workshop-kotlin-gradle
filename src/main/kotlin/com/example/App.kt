320c4e962812436fc2cd781c4d7cf49c55ddc811
sebastian.schmidt <bastman77@gmail.com>
2018-07-21 11:17:59 +0200

    initial

---
+++ src/main/kotlin/com/example/App.kt
@@ -0,0 +1,10 @@
+package com.example
+
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World (kotlin)")
+        }
+    }
+}
