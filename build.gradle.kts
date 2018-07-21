8bf5c17f56dd363cd41597ff2f43fda5b6955565
sebastian.schmidt <bastman77@gmail.com>
2018-07-21 11:49:55 +0200

    initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.2.51"
-
-    // Apply the application plugin to add support for building an application
     application
 }
 
@@ -12,18 +10,21 @@ group = "com.example"
 version = "1.0-SNAPSHOT"
 
 application {
-
-    // Define the main class for the application
     mainClassName = "com.example.App"
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
+    compile("com.google.guava:guava:23.0")
+
+    // Use JUnit test framework
+    testCompile("junit:junit:4.12")
 }
 
 configure<JavaPluginConvention> {
